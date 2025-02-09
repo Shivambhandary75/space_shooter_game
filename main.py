182ac95c9d4b6c7570823827aee025f6d5a4e323
prasadnaik12p <mdbprasad223@gmail.com>
2025-02-09 21:09:22 +0530

    Add Background

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import pygame
-
 pygame.init()
 
+
+#screen setup
 screen=pygame.display.set_mode((800,600))
 pygame.display.set_caption("SPACE SHOOTER SAGA")
 icon=pygame.image.load("game_icon.png")
 pygame.display.set_icon(icon)
+background = pygame.image.load('background.jpg')
+playerimg = pygame.image.load('spaceship.png')
+
+# Player Position,Centering the spaceship
+playerX = 370
+playerY = 480
+
+def player(x, y):
+    screen.blit(playerimg,(x, y))
+
 running=True
 while running:
+    screen.blit(background,(0,0))
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-print("test message")
+
+    player(playerX, playerY)
+    pygame.display.update()
