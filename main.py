642d86bdfd14864c530a73ad597b9636080a951b
Shivambhandary75 <shivambhandary5@gmail.com>
2025-02-09 17:37:44 +0530

    added icon and game name

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pygame
 pygame.init()
 
 screen=pygame.display.set_mode((800,600))
+pygame.display.set_caption("SPACE SHOOTER SAGA")
+icon=pygame.image.load("game_icon.png")
+pygame.display.set_icon(icon)
 running=True
 while running:
     for event in pygame.event.get():
