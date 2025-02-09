d30fae978d62c30bb8c343ecf0057a36439e924e
Shivambhandary75 <shivambhandary5@gmail.com>
2025-02-09 22:19:02 +0530

    added spaceship

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ pygame.init()
 
 
 #screen setup
+#window  size and is resizable now
 screen=pygame.display.set_mode((800,600),pygame.RESIZABLE)
 pygame.display.set_caption("SPACE SHOOTER SAGA")
 icon=pygame.image.load("game_icon.png")
 pygame.display.set_icon(icon)
 background = pygame.image.load('background.jpg')
 # pygame.display.
-# playerimg = pygame.image.load('spaceship.png')
+player_img = pygame.image.load('spaceship.png')
 
 # Player Position,Centering the spaceship
-# playerX = 370
-# playerY = 480
-
-# def player(x, y):
-#     screen.blit(playerimg,(x, y))
+playerX = 370
+playerY = 520
 
 running=True
 while running:
-    screen.blit(background,(0,0))
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-
-    # player(playerX, playerY)
+    screen.blit(background, (0, 0))
+    screen.blit(player_img,(playerX, playerY))
     pygame.display.update()
